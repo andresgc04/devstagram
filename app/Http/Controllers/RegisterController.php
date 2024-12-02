ee47f3f0c68be9f3b044c9d37b46884ca1bed2b7
Andres Guerrero <andresgc1997@outlook.com>
2024-12-02 00:10:45 -0400

    New Feature
    Evitar UserName Duplicados

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -21,6 +21,9 @@ class RegisterController extends Controller
 
         //dd($request->get('username'));
 
+        // Modificar el request:
+        $request->request->add(['username' => Str::slug($request->username)]);
+
         //Validación:
         $request->validate([
             'name' => 'required|max:50',
@@ -31,7 +34,7 @@ class RegisterController extends Controller
 
         User::create([
             'name' => $request->name,
-            'username' => Str::slug($request->username),
+            'username' => $request->username,
             'email' => $request->email,
             'password' => Hash::make($request->password)
         ]);
