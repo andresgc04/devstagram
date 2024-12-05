0f0383ca8b9424394c46ea9fd8a7f66b99da6a45
Andres Guerrero <andresgc1997@outlook.com>
2024-12-05 01:01:26 -0400

    New Feature
    Revisar si las credenciales son correctas, e imprimir mensaje de error

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
@@ -18,5 +19,11 @@ class LoginController extends Controller
             'email' => 'required|email',
             'password' => 'required'
         ]);
+
+        if (!Auth::attempt($request->only('email', 'password'))) {
+            return back()->with('mensaje', 'Credenciales Incorrectas');
+        };
+
+        return redirect()->route('posts.index');
     }
 }
