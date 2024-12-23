d9aa7897570b8b2068456698974d274e41d4b5ac
Andres Guerrero <andresgc1997@outlook.com>
2024-12-22 23:08:21 -0400

    New Feature
    Agregar la imagen al Dropzone si la validacion falla pero tenemos una imagen.

--- resources/js/app.js
+++ resources/js/app.js
@@ -9,6 +9,29 @@ const dropzone = new Dropzone("#dropzone", {
     dictRemoveFile: "Borrar Archivo",
     maxFiles: 1,
     uploadMultiple: false,
+
+    init: function () {
+        if (document.querySelector('[name="imagen"]').value.trim()) {
+            const imagenPublicada = {};
+            imagenPublicada.size = 1234;
+
+            imagenPublicada.name =
+                document.querySelector('[name="imagen"]').value;
+
+            this.options.addedfile.call(this, imagenPublicada);
+
+            this.options.thumbnail.call(
+                this,
+                imagenPublicada,
+                `/uploads/${imagenPublicada.name}`
+            );
+
+            imagenPublicada.previewElement.classList.add(
+                "dz-success",
+                "dz-complete"
+            );
+        }
+    },
 });
 
 // dropzone.on("sending", function (file, xhr, formData) {
@@ -23,4 +46,6 @@ dropzone.on("success", function (file, response) {
 //     console.log(message);
 // });
 
-dropzone.on("removedfile", function () {});
+dropzone.on("removedfile", function () {
+    document.querySelector('[name="imagen"]').value = "";
+});
