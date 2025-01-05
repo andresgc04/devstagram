8f422db8523a68110edcc1bf869b64fd40f05538
Andres Guerrero <andresgc1997@outlook.com>
2025-01-04 23:12:57 -0400

    New Feature
    Agregando TailWind A La Paginacion

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -11,7 +11,7 @@ class PostController extends Controller
 {
     public function index(User $user)
     {
-        $posts = Post::where('user_id', $user->id)->paginate(5);
+        $posts = Post::where('user_id', $user->id)->paginate(10);
 
         return view('dashboard', [
             'user' => $user,
