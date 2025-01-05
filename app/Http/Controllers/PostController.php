7ee1b4a1f6f76b6303f49b89967182fd9a55c055
Andres Guerrero <andresgc1997@outlook.com>
2025-01-04 22:32:10 -0400

    New Feature
    Agregando paginacion a las publicaciones

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -11,7 +11,7 @@ class PostController extends Controller
 {
     public function index(User $user)
     {
-        $posts = Post::where('user_id', $user->id)->get();
+        $posts = Post::where('user_id', $user->id)->paginate(5);
 
         return view('dashboard', [
             'user' => $user,
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -43,6 +43,10 @@
                     </div>
                 @endforeach
             </div>
+
+            <div>
+                {{ $posts->links() }}
+            </div>
         @else
             <p class="text-gray-600 uppercase text-sm text-center font-bold">No hay posts</p>
         @endif
