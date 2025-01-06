8427c434b7096e334a94c2da89472fc73ba0f2d2
Andres Guerrero <andresgc1997@outlook.com>
2025-01-05 21:46:49 -0400

    New Feature
    Mejorando la estructura de las URLS

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -57,7 +57,7 @@ class PostController extends Controller
         return redirect()->route('posts.index', Auth::user()->username);
     }
 
-    public function show(Post $post)
+    public function show(User $user, Post $post)
     {
         return view(
             'posts.show',
