2da16bb1b0308d73ac49405a2e1a9767eb92b6a8
Andres Guerrero <andresgc1997@outlook.com>
2025-01-25 13:49:11 -0400

    New Feature
    Almacenando los comentarios

--- app/Http/Controllers/ComentarioController.php
+++ app/Http/Controllers/ComentarioController.php
@@ -2,16 +2,29 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Comentario;
+use App\Models\Post;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class ComentarioController extends Controller
 {
-    public function store()
+    public function store(Request $request, User $user, Post $post)
     {
         //Validar:
+        $request->validate([
+            'comentario' => 'required|max:255'
+        ]);
 
         //Almacenar el resultado:
+        Comentario::create([
+            'user_id' => Auth::user()->id,
+            'post_id' => $post->id,
+            'comentario' => $request->comentario
+        ]);
 
         //Imprimir un mensaje:
+        return back()->with('mensaje', 'Comentario Realizado Correctamente');
     }
 }
